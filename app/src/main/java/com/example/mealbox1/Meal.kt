30850a927e520ed84837b7ea874327a747651976
SeungHyun <67039023+gusanans218@users.noreply.github.com>
2021-06-22 23:46:23 +0900

    <Create> 기본 파일 + Splash + ViewPager

---
+++ app/src/main/java/com/example/mealbox1/Meal.kt
@@ -0,0 +1,6 @@
+package com.example.mealbox1
+
+data class Meal (
+    val meal:String,
+    val name:String
+)
--- app/src/main/java/com/example/mealbox1/menuAdapter.kt
+++ app/src/main/java/com/example/mealbox1/menuAdapter.kt
@@ -1,28 +1,43 @@
 package com.example.mealbox1
 
+import android.view.LayoutInflater
+import android.view.TextureView
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class menuAdapter(fragmentActivity: ViewPagerFragment): FragmentStateAdapter(fragmentActivity) {
+class menuAdapter(
+    val meal:List<Meal>
+): RecyclerView.Adapter<menuAdapter.menuViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)=
+        menuViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.activity_menu,parent,false)
+        )
 
-    var fragments : ArrayList<Fragment> = ArrayList()
-
-    override fun getItemCount(): Int {
-        return fragments.size
+    override fun onBindViewHolder(holder: menuViewHolder, position: Int) {
+        TODO("Not yet implemented")
     }
 
-    override fun createFragment(position: Int): Fragment {
-        return fragments[position]
+    override fun getItemCount() = meal.size
+
+    class menuViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+        private val mealTextView:TextureView=itemView.findViewById(R.id.mealTextView)
+        private val nameTextView:TextureView=itemView.findViewById(R.id.nameTextView)
+
+
+        fun bind(meal:Meal){
+            mealTextView.text = meal.meal
+            nameTextView.text = meal.name
+
+        }
+
     }
 
-    fun addFragment(fragment: Fragment) {
-        fragments.add(fragment)
-        notifyItemInserted(fragments.size-1)
-    }
 
-    fun removeFragment() {
-        fragments.removeLast()
-        notifyItemRemoved(fragments.size)
-    }
 
 }
